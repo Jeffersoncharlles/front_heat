95321e1b95020f112e6c245fb7eedf14e985ddd3
jeffersoncharlles <contato@jefferdeveloper.com>
2021-10-19 21:03:00 -0300

    add axios start get list3  commit

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,8 +1,27 @@
 import styles from './styles.module.scss';
 
 import logoImg from '../../assets/logo.svg'
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name:string;
+        avatar_url:string;
+    }
+}
 
 export const MessageList = () => {
+    const [messages,setMessages] = useState<Message[]>([]);
+
+    useEffect(()=>{
+        api.get<Message[]>('messages/last3').then(res =>{
+            setMessages(res.data);
+        })
+    },[]);
+
 
     return (
         <div className={styles.messageListWrapper}>
@@ -19,25 +38,6 @@ export const MessageList = () => {
                     </div>
                 </li>
 
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/jeffersoncharlles.png" alt="Jefferson Charlles"  />
-                        </div>
-                        <span>Jefferson Charlles</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/jeffersoncharlles.png" alt="Jefferson Charlles"  />
-                        </div>
-                        <span>Jefferson Charlles</span>
-                    </div>
-                </li>
             </ul>
 
         </div>
