382bc4f3abc738cec55eb5c3c7c09ef3f6fe0165
jeffersoncharlles <contato@jefferdeveloper.com>
2021-10-19 22:44:04 -0300

    handlesendmessage commit

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,10 +1,26 @@
-import { useContext, useState } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import { VscGithubInverted, VscSignOut } from 'react-icons/vsc';
 import { AuthContext } from '../../contexts/auth';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
 
 export const SendMessageForm = () => {
     const {user, signOut} = useContext(AuthContext);
+    const [message, setMessage] = useState('');
+
+    const handleSendMessage = async (event: FormEvent)=>{
+
+        event.preventDefault();
+        /*nao enviar usuario para nenhuma action*/
+
+        if (!message.trim()) {
+            return;
+            /*se o texto tiver vazio mesmo 
+            removendo o espaco nao fazer nada*/
+        }
+
+        await api.post('messages', {message});
+    }
     
 
     return (
@@ -15,28 +31,34 @@ export const SendMessageForm = () => {
             </button>
 
             <header className={styles.userInformation}>
+
                 <div className={styles.userImage}>
                     <img src={user?.avatar_url} alt={user?.name} />
                 </div>
 
                 <strong className={styles.userName}>{user?.name}</strong>
+
                 <span className={styles.userGithub}>
                     <VscGithubInverted  size={16}/>
                     {user?.login}
                 </span>
+
             </header>
 
-            <form className={styles.sendMessageForm}>
+            <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
                 <label htmlFor="message">Mensagem</label>
-                <textarea 
-                name='message'
-                id='message'
-                placeholder='Qual sua expectativa para o evento?'
                 
+                <textarea 
+                    onChange={e=>setMessage(e.target.value)}/*pegando valor digitado e colocando no setMessage*/
+                    name='message'
+                    id='message'
+                    placeholder='Qual sua expectativa para o evento?'
+                    value={message}
                 />
 
                 <button type='submit'>Enviar mensagem</button>
             </form>
+
         </div>
     )
 }
