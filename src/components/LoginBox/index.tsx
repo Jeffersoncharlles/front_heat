970998a92f4df02eeb0b1125e57fedcd8484b8ff
jeffersoncharlles <contato@jefferdeveloper.com>
2021-10-19 21:50:42 -0300

    context api ok  commit

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,53 +1,14 @@
 import styles from './styles.module.scss';
 import { VscGithubInverted } from 'react-icons/vsc';
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
-
-type AuthResponse = {
-    token: string;
-    user: {
-        id: string;
-        avatar_url:string;
-        name:string;
-        login: string;
-    }
-}
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../contexts/auth';
 
 
 export const LoginBox = () => {
+    const {signInUrl, user} = useContext(AuthContext);
     const [] = useState();
-    const signInUrl =`https://github.com/login/oauth/authorize?scope=user&client_id=fc0ab1a4cf7ccb0991c2`;
-
-    const signIn = async (githubCode :string)=>{
-       const response = await api.post<AuthResponse>('authenticate',{
-            code:githubCode,
-        })
-
-        const {token, user} = response.data;
-
-        /*token salvar no storage do navegador*/
-        localStorage.setItem('@dowhile2021:token', token);
-
-        // console.log(user);
-    }
-
-
-    useEffect(()=>{
-        /*buscar url da app*/
-        const url = window.location.href;
-        /*verificar se inclui code*/
-        const hasGithubCode = url.includes('?code=');
-
-        if (hasGithubCode) {
-            const [urlWithoutCode, githubCode] = url.split('?code=');
-
-            /*limpar url usuario*/ /*assim usuario nao ve o code*/
-            window.history.pushState({},'',urlWithoutCode);
-
-            signIn(githubCode);
-        }
-    },[]);
-
+    
+    
 
     return (
         <div className={styles.loginBoxWrapper}>
--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,6 +1,32 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode,useEffect, useState } from "react";
+import { api } from "../services/api";
 
-const AuthContext = createContext(null)
+
+type User = {
+    id: string;
+    name: string;
+    login: string;
+    avatar_url:string;
+}
+
+/*usuario ou e do tipo user ou null*/
+/*se nao tiver logado vai ta null*/
+type AuthContextData = {
+    user: User | null;
+    signInUrl: string;
+}
+
+type AuthResponse = {
+    token: string;
+    user: {
+        id: string;
+        avatar_url:string;
+        name:string;
+        login: string;
+    }
+}
+
+const AuthContext = createContext({} as AuthContextData);
 
 
 /*react node e qualquer coisa aceitavel pelo react tudo*/
@@ -10,12 +36,50 @@ type AuthProvider = {
 
 
 const AuthProvider = (props: AuthProvider)=>{
+    /*se eu tiver logado user se nao null <User | null>*/
+    const [user, setUser] = useState<User | null>(null);
+
+    const signInUrl =`https://github.com/login/oauth/authorize?scope=user&client_id=fc0ab1a4cf7ccb0991c2`;
+
+    const signIn = async (githubCode :string)=>{
+       const response = await api.post<AuthResponse>('authenticate',{
+            code:githubCode,
+        })
+
+        const {token, user} = response.data;
+
+        /*token salvar no storage do navegador*/
+        localStorage.setItem('@dowhile2021:token', token);
+
+        // console.log(user);
+        setUser(user);
+    }
+
+
+    useEffect(()=>{
+        /*buscar url da app*/
+        const url = window.location.href;
+        /*verificar se inclui code*/
+        const hasGithubCode = url.includes('?code=');
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=');
+
+            /*limpar url usuario*/ /*assim usuario nao ve o code*/
+            window.history.pushState({},'',urlWithoutCode);
+
+            signIn(githubCode);
+        }
+    },[]);
+
+
+
     return (
-        <AuthContext.Provider value={null}>
+        <AuthContext.Provider value={ { signInUrl,  user} }>
             {props.children}
         </AuthContext.Provider>
 
     );
 }
 
-export { AuthProvider}
+export { AuthProvider, AuthContext} ;
